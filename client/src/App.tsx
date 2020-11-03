a90db659ec6eac82f9d01d3c509422e2d83fd999
Ian Trent <ian.m.trent@gmail.com>
2020-11-02 22:26:03 -0500

    feat: rendering url components with fake data for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Url } from './components/url';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>URL Shortener</h1>
+      {Array.from(Array(10), (el, i) => {
+        return <Url redirect="efg" shortId="abc" />;
+      })}
     </div>
   );
 }
